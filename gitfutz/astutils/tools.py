4f45b7b7a3e70ee14d814ec4852b9b65203b6aff
Tim Henderson <tim.tadh@gmail.com>
2012-05-12 20:49:13 -0400

    Added a gitfutz/astutils/tools module.
    The tools module/package will centralize functions related to ast
    manipulation and comparison. For instance it may contain functions for
    extracting "interesting" subtrees. Immediately it will be for comparing
    AST using tree edit distance.
    
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ gitfutz/astutils/tools.py
@@ -0,0 +1,14 @@
+!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
+import os, sys
+import ast
+
+from betterast import Node
+
+from gitfutz.io import log, output
+
+
