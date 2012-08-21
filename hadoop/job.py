78b3a3687f7f9d72598c9e5f037972c02d5ac2c7
Tim Henderson <tim.tadh@gmail.com>
2012-08-21 15:56:29 -0400

    stabilizing the job
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- hadoop/job.py
+++ hadoop/job.py
@@ -4,12 +4,13 @@
 #Email: tim.tadh@hackthology.com
 #For licensing see the LICENSE file in the top level directory.
 
-import sys, os, difflib
+import sys, os, difflib, random
 
 import pygit2
 from mrjob.job import MRJob
 from mrjob.util import bash_wrap
 from editdist import distance as strdist
+from sklearn import cluster
 
 from gitfutz.astutils import tools
 
@@ -21,7 +22,9 @@ def quick_strdist(a,b):
 def collect_files(files, tree, parents):
     for entry in tree:
         name = entry.name.encode('utf8')
-        eobj = entry.to_object()
+        try: eobj = entry.to_object()
+        except:
+            continue
         if isinstance(eobj, pygit2.Tree): 
             collect_files(files, eobj, parents+[name])
         else:
@@ -32,7 +35,6 @@ class Sequence(MRJob):
     def steps(self):
         return [
           self.mr(mapper=self.commits),
-          self.mr(mapper=self.commit_files),
           self.mr(
             mapper=self.commit_stats, 
             reducer=self.sum_stats,
@@ -40,12 +42,16 @@ class Sequence(MRJob):
           self.mr(
             mapper=self.normalize_stats_mapper, 
             reducer=self.normalize_stats_reducer),
+          self.mr(
+            reducer=self.cluster_authors),
         ]
-
-    def get_commit(self, path, chex):
-        repo = pygit2.Repository(path)
-        commit = repo[chex.decode('hex')]
-        return commit
+    def configure_options(self):
+        super(Sequence, self).configure_options()
+        self.add_passthrough_option('--sample', default=False,
+            action='store_true',
+            help='Should sampling occur?')
+        self.add_passthrough_option('--sample-threshold', type='float',
+            default=.4, help='What percentage of commits should be sampled?')
 
     def commits(self, _, url):
         path = os.path.join(SUBJECTS_DIR, 
@@ -53,18 +59,22 @@ class Sequence(MRJob):
         repo = pygit2.Repository(path)
         head = repo.lookup_reference('HEAD').resolve()
         for commit in repo.walk(head.oid, pygit2.GIT_SORT_REVERSE):
-            yield path, commit.hex
+            if not self.options.sample: 
+                yield path, commit.hex
+            else:
+                if random.random() < self.options.sample_threshold:
+                    yield path, commit.hex
+                else:
+                    print 'skipped', path, commit.hex
 
-    def commit_files(self, path, chex):
-        commit = self.get_commit(path, chex)
+    def commit_stats(self, path, chex):
+        repo = pygit2.Repository(path)
+        try:
+            commit = repo[chex.decode('hex')]
+        except:
+            return
         merge = 1 if len(commit.parents) > 1 else 0
-        yield (path, chex), {
-            'file_name': None,
-            'parent_file': None,
-            'commit_file': None,
-            'merges': merge,
-            'commits': 1
-        }
+        yield (path, commit.author.email), (1, merge, 0.0)
         files1 = dict()
         collect_files(files1, commit.tree, list())
         for parent in commit.parents:
@@ -73,39 +83,37 @@ class Sequence(MRJob):
             shared = set(files1.keys()) & set(files2.keys())
             for key in shared:
                 if files1[key] == files2[key]: continue
-                yield (path, chex), {
-                    'file_name': key,
-                    'parent_file': files1[key],
-                    'commit_file': files2[key],
-                    'merges': 0,
-                    'commits': 0
-                }
-
-    def commit_stats(self, pc, d):
-        path, chex = pc
-        commit = self.get_commit(path, chex)
-        if d['file_name'] is None: 
-            yield (path, commit.author.email), (d['commits'], d['merges'], 0.0)
-        else:
-            repo = pygit2.Repository(path)
-            f1 = repo[d['parent_file'].decode('hex')].data.replace('\r', '')
-            f2 = repo[d['commit_file'].decode('hex')].data.replace('\r', '')
-            yield (path, commit.author.email), (0, 0, quick_strdist(f1,f2))
+                try:
+                    f1 = repo[files1[key].decode('hex')].data.replace('\r', '')
+                    f2 = repo[files2[key].decode('hex')].data.replace('\r', '')
+                except:
+                    continue
+                try:
+                    ## "binary" file check
+                    f1 = f1.decode('utf8').encode('utf8')
+                    f2 = f1.decode('utf8').encode('utf8')
+                except UnicodeDecodeError:
+                    continue
+                yield (path, commit.author.email), (0, 0, quick_strdist(f1,f2))
 
     def sum_stats(self, pa, cms):
         path, author = pa
-        cms = [cm for cm in cms]
-        commits = map(lambda x: x[0], cms)
-        merges = map(lambda x: x[1], cms)
-        sdists = map(lambda x: x[2], cms)
-        yield (path, author), (float(sum(commits)), float(sum(merges)),
-            float(sum(sdists)))
+        t_commits = 0.0
+        t_merges = 0.0
+        t_sdist = 0.0
+        for commits, merges, sdist in cms:
+            t_commits += commits
+            t_merges += merges
+            t_sdist += sdist
+        yield (path, author), (float(t_commits), float(t_merges),
+            float(t_sdist))
 
     def normalize_stats_mapper(self, pa, cms):
         path, author = pa
         yield path, (author, cms[0], cms[1], cms[2])
 
     def normalize_stats_reducer(self, path, acms):
+        def div(q, d): return q/d if d != 0.0 else 0.0
         t_commits = 0.0
         t_merges = 0.0
         t_sdist = 0.0
@@ -116,8 +124,35 @@ class Sequence(MRJob):
             t_merges += merges
             t_sdist += sdist
         for author, commits, merges, sdist in c_acms:
-            yield (path, author), ((commits/t_commits, merges/t_merges,
-                sdist/t_sdist), (commits, merges, sdist))
+            yield path, (author, (
+              (div(commits, t_commits), div(merges, t_merges), div(sdist, t_sdist)), 
+              (commits, merges, sdist)
+            ))
+    
+    def cluster_authors(self, path, acms):
+        acms = tuple(i for i in acms)
+        try:
+            est = cluster.KMeans(k=min(max(len(acms)/2, 2), 4))
+            est.fit([cms[0] for author, cms in acms])
+            groups = tuple((author, est.predict(cms[0]).tolist()[0], cms)
+                        for author, cms in acms)
+        except:
+            groups = tuple((author, -1, cms) for author, cms in acms)
+        groups = sorted(groups, key=lambda x: x[1])
+        for author, group, cms in groups:
+            yield (path, author), {
+              'group': group,
+              'raw': {
+                'commits':cms[1][0],
+                'merges':cms[1][1],
+                'sdist':cms[1][2],
+              },
+              'normalized': {
+                'commits':cms[0][0],
+                'merges':cms[0][1],
+                'sdist':cms[0][2],
+              },
+            }
 
 if __name__ == '__main__':
     Sequence.run()
